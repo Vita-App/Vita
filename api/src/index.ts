4eb56da6c62a241a2eaaf875e9522b369da19f67
Lalit <lalitkumarsingh3716@gmail.com>
2022-03-05 11:02:15 +0530

    feat: added testing

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,48 +1,20 @@
-import express from 'express';
-import chalk from 'chalk';
-import http from 'http';
-import { port } from './config/keys';
-import apiRoutes from './routes/apiRoutes';
 import connectDB from './config/connectDatabase';
-import './Models/User';
+import createServer from './app';
 import socketioService from './service/socket-io-service';
-import cors from 'cors';
-import { CORS_REGEX } from './config/keys';
-// import passport from 'passport';
-// import useMiddleWare from './middleware/index';
+import { port } from './config/keys';
+import chalk from 'chalk';
 
-// import passportService from './service/passport';
-// passportService(passport);
+const { httpServer } = createServer();
 
-const app = express();
-const httpServer = new http.Server(app);
+connectDB()
+  .then(() => {
+    socketioService(httpServer);
 
-connectDB();
-// useMiddleWare(app);
-
-app.use(
-  cors({
-    origin: new RegExp(CORS_REGEX),
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    allowedHeaders: 'Content-Type',
-    credentials: true, // allow session cookies from browser to pass throught
-  }),
-);
-
-app.set('trust proxy', 1);
-
-// app.use(passport.initialize());
-// app.use(passport.session());
-
-app.use('/api', apiRoutes);
-socketioService(httpServer);
-
-app.use('/', (req, res) =>
-  res.send(`
-  <h1>Server is Running :)))</h1>
-`),
-);
-
-httpServer.listen(port, () =>
-  console.log(chalk.blueBright(`Express Server listening to port ${port}`)),
-);
+    httpServer.listen(port, () =>
+      console.log(chalk.blueBright(`Express Server listening to port ${port}`)),
+    );
+  })
+  .catch((err) => {
+    console.log(chalk.redBright(err));
+    process.exit();
+  });
