b28a7dc0d0b7275bfbe4a3a5940ea48b1e715513
Lalit <lalitkumarsingh3716@gmail.com>
2022-03-06 18:05:23 +0530

    Enhance: Footer

--- client/src/components/Footer/Footer.tsx
+++ client/src/components/Footer/Footer.tsx
@@ -1,17 +1,36 @@
 import React from 'react';
-import Grid from '@mui/material/Grid';
+import Stack from '@mui/material/Stack';
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
 import Link from '@mui/material/Link';
 
 const Footering = () => (
-    <footer>
-        <Container maxWidth="lg">
-            <Grid container spacing={5}>
-                <Grid item xs={12} sm={4}></Grid>
-            </Grid>
-        </Container>
-    </footer>
+  <Box component="footer" bgcolor="white">
+    <Container maxWidth="lg" sx={{ p: 2 }}>
+      <Stack>
+        <Stack
+          direction={{ md: 'row' }}
+          justifyContent="space-between"
+          alignItems="center">
+          <Box>Vita Logo and Text</Box>
+          <Box>Social Media Icons</Box>
+        </Stack>
+        <Stack
+          direction={{ md: 'row' }}
+          justifyContent="space-between"
+          alignItems="center">
+          <Stack direction="column">
+            <Box>Links</Box>
+            <p>Copyright</p>
+          </Stack>
+          <Stack direction="column">
+            <Box>Additional Links like join vita, faqs</Box>
+            <Box>Contact Us, Privacy Policy, etc...</Box>
+          </Stack>
+        </Stack>
+      </Stack>
+    </Container>
+  </Box>
 );
 
 export default Footering;
