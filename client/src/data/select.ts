3afd890245fcf752f9e10eda7d9df413aeaae96f
Lalit <lalitkumarsingh3716@gmail.com>
2022-03-26 23:31:08 +0530

    Adding Complete Signup Route (#64)
    * Improving Workflows
    
    * feat(ui): Added Complete Signup Route
    
    * feat(ui): Added Complete Signup Route
    
    * feat(ui): Added Experience Form with validations
    
    * Preserving form data across all stepper components
    
    * Added Profile Form
    
    * Added react-select for some fields
    
    * Added Integrations Form UI
    
    * fix: Making remove image button working
    
    * small fixes
    
    Co-authored-by: Rishabh Malhotra <rishabhmalhotraa01@gmail.com>

--- client/src/data/select.ts
+++ client/src/data/select.ts
@@ -47,3 +47,145 @@ export const expertiseOptions = [
     label: 'Software Development',
   },
 ];
+
+export const companyOptions = [
+  {
+    value: 'Facebook',
+    label: 'Facebook',
+  },
+  {
+    value: 'Apple',
+    label: 'Apple',
+  },
+  {
+    value: 'Amazon',
+    label: 'Amazon',
+  },
+  {
+    value: 'Netflix',
+    label: 'Netflix',
+  },
+  {
+    value: 'Google',
+    label: 'Google',
+  },
+];
+
+export const interestOptions = [
+  {
+    value: 'AI/ML',
+    label: 'AI/ML',
+  },
+  {
+    value: 'Backend Development',
+    label: 'Backend Development',
+  },
+  {
+    value: 'C++',
+    label: 'C++',
+  },
+  {
+    value: 'Competitive Coding',
+    label: 'Competitive Coding',
+  },
+  {
+    value: 'Frontend Development',
+    label: 'Frontend Development',
+  },
+  {
+    value: 'Fullstack Development',
+    label: 'Fullstack Development',
+  },
+  {
+    value: 'Golang',
+    label: 'Golang',
+  },
+  {
+    value: 'Graphic Design',
+    label: 'Graphic Design',
+  },
+  {
+    value: 'Java',
+    label: 'Java',
+  },
+  {
+    value: 'JavaScript',
+    label: 'JavaScript',
+  },
+  {
+    value: 'Android Development',
+    label: 'Android Development',
+  },
+  {
+    value: 'Open Source',
+    label: 'Open Source',
+  },
+  {
+    value: 'Python',
+    label: 'Python',
+  },
+  {
+    value: 'React',
+    label: 'React',
+  },
+  {
+    value: 'DevOps',
+    label: 'DevOps',
+  },
+  {
+    value: 'Cloud Computing',
+    label: 'Cloud Computing',
+  },
+  {
+    value: 'UI/UX',
+    label: 'UI/UX',
+  },
+];
+
+export const leadershipOptions = [
+  {
+    label: 'Leadership Option 1',
+    value: 'Leadership Option 1',
+  },
+  {
+    label: 'Leadership Option 2',
+    value: 'Leadership Option 2',
+  },
+];
+
+export const careerOptions = [
+  {
+    label: 'Career Option 1',
+    value: 'Career Option 1',
+  },
+  {
+    label: 'Career Option 2',
+    value: 'Career Option 2',
+  },
+  {
+    label: 'Career Option 3',
+    value: 'Career Option 3',
+  },
+];
+
+export const skillOptions = [
+  {
+    label: 'Skill Option 1',
+    value: 'Skill Option 1',
+  },
+  {
+    label: 'Skill Option 2',
+    value: 'Skill Option 2',
+  },
+];
+
+export const jobSearchOptions = [
+  {
+    label: 'Job Search Option 1',
+    value: 'Job Search Option 1',
+  },
+  {
+    label: 'Job Search Option 2',
+    value: 'Job Search Option 2',
+  },
+];
