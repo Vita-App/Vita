469a580aad8e1067b79a8271e6bab1d2658251f7
Lalit <lalitkumarsingh3716@gmail.com>
2022-04-15 11:10:06 +0530

    Fixing Some backend Errors and cleaning a bit.  (#85)
    * Bootstraping Admin Client
    
    * Fixing Some Errors
    
    * Removed admin folder

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,36 +1,19 @@
 import express from 'express';
 import chalk from 'chalk';
 import http from 'http';
-import { port, COOKIE_KEYS, CLIENT_DOMAIN, DATABASE_URL } from './config/keys';
+import { port } from './config/keys';
 import apiRoutes from './routes/apiRoutes';
 import connectDB from './config/connectDatabase';
 import './Models/User';
-import socketioService from './service/socket-io-service';
-import cors from 'cors';
-import { CORS_REGEX } from './config/keys';
-import useMiddleWare from './middleware/index';
 import './config/passport-config';
-import expressSession from 'express-session';
-import passport from 'passport';
-import MongoStore from 'connect-mongo';
+import socketioService from './service/socket-io-service';
+import useMiddleWare from './middleware/index';
 
 const app = express();
 const httpServer = new http.Server(app);
 
+connectDB();
 useMiddleWare(app);
-const connection = connectDB();
-
-app.use(expressSession({
-  secret: COOKIE_KEYS[0],
-  resave: false,
-  saveUninitialized: false,
-  store: MongoStore.create({ mongoUrl: DATABASE_URL }),
-  cookie: {
-    domain: CLIENT_DOMAIN,
-    maxAge: 24*60*60*1000,
-    // secure: true
-  } 
-}))
 
 app.use('/api', apiRoutes);
 socketioService(httpServer);
