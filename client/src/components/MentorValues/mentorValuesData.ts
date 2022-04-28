0fac86540d9b80653470363044fc41f6396ae471
indraantoor <indraantoor@gmail.com>
2022-04-28 19:17:16 +0530

    Add: mentor values data

---
+++ client/src/components/MentorValues/mentorValuesData.ts
@@ -0,0 +1,26 @@
+const freeOfChargeValueData = {
+  headline: 'Free of charge',
+  description:
+    'Mentorship is a two-way street, and for mentors, helping others succeed is rewarding in itself.',
+  image: '/Illustration_FreeOfCharge.svg',
+};
+
+const vettedAndVerfiedValueData = {
+  headline: 'Vetted & Verified',
+  description:
+    'Mentors require a minimum of 5 years of experience in their respective industries to apply on the platform and a passion for mentoring.',
+  image: '/Illustration_VettedVerified.svg',
+};
+
+const diverseAndInclusiveValueData = {
+  headline: 'Diverse & Inclusive',
+  description:
+    'Over 4000 mentors from 60+ different countries, from various industries, and all walks of life.',
+  image: '/Illustration_Diversity.svg',
+};
+
+export const mentorValuesData = [
+  freeOfChargeValueData,
+  vettedAndVerfiedValueData,
+  diverseAndInclusiveValueData,
+];
