098417569164aa38a8c211e22a97af1065b8c9a1
Ankur Raj <64456355+ancoreraj@users.noreply.github.com>
2022-07-10 22:07:25 +0530

    created CalenderCredential Model to store refresh token

---
+++ api/src/Models/CalendarCredentials.ts
@@ -0,0 +1,9 @@
+import { Schema, model } from 'mongoose';
+import { CalenderCredentialsSchemaTypes } from '../types';
+
+const CalendarCredentialsSchema = new Schema<CalenderCredentialsSchemaTypes>({
+  email : { type: String},
+  refresh_token: {type : String}
+});
+
+export const CalendarCredentialsModel = model('CalendarCredentials', CalendarCredentialsSchema);
