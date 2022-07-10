78f64aaac5a58f7b1dfe35a363da3131a31e93f3
Ankur Raj <64456355+ancoreraj@users.noreply.github.com>
2022-07-10 22:11:01 +0530

    created function to create a calender event

---
+++ api/src/utils/createCalendarEvent.ts
@@ -0,0 +1,63 @@
+import { google } from 'googleapis';
+import { CalendarCredentialsModel } from '../Models/CalendarCredentials';
+const { OAuth2 } = google.auth;
+
+import { CalendarOptionTypes } from '../types';
+
+const createCalenderEvent = async (options: CalendarOptionTypes) => {
+
+  let oAuth2Client = new OAuth2(
+    process.env.GOOGLE_KEY_CLIENTID,
+    process.env.GOOGLE_KEY_CLIENTSECRET,
+  );
+
+  const refresh_token: any = await CalendarCredentialsModel.find({})
+
+  oAuth2Client.setCredentials({
+    refresh_token: refresh_token[0],
+  });
+
+  let calendar: any = google.calendar({ version: 'v3', auth: oAuth2Client });
+
+  const event = {
+    summary: options.summary,
+    location: 'Virtual / Google Meet',
+    description: options.description,
+    start: {
+      dateTime: options.startTime,
+      timeZone: 'Asia/Kolkata',
+    },
+    end: {
+      dateTime: options.endTime,
+      timeZone: 'Asia/Kolkata',
+    },
+    attendees: options.attendeesEmails,
+    reminders: {
+      useDefault: false,
+      overrides: [
+        { method: 'email', minutes: 24 * 60 },
+        { method: 'popup', minutes: 10 },
+      ],
+    },
+    conferenceData: {
+      createRequest: {
+        conferenceSolutionKey: {
+          type: 'hangoutsMeet',
+        },
+        requestId: 'coding-calendar-demo',
+      },
+    },
+  };
+
+  const response = await calendar.events.insert({
+    calendarId: 'primary',
+    resource: event,
+    conferenceDataVersion: 1,
+  });
+
+  console.log(response)
+
+  return response.data.hangoutLink;
+};
+
+export default createCalenderEvent;
