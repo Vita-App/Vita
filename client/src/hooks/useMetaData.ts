2dbc55ffa9f5c89e5d5d6d5ba2748eaf303f79d9
Lalit <lalitkumarsingh3716@gmail.com>
2022-08-13 14:25:05 +0530

    Changing MetaData based on env variables

---
+++ client/src/hooks/useMetaData.ts
@@ -0,0 +1,57 @@
+import { useEffect } from 'react';
+import { APP_NAME, ASSET_FOLDER } from 'config.keys';
+
+const description = `${APP_NAME} connects college graduates, alumni, and faculty to revive the culture of mentorship and foster a more connected and engaged community.`;
+
+const createLinkElements = () => {
+  const link = document.createElement('link');
+  link.rel = 'icon';
+  link.href = `/${ASSET_FOLDER}/icon.ico`;
+
+  const appleLink = document.createElement('link');
+  appleLink.rel = 'apple-touch-icon';
+  appleLink.href = `/${ASSET_FOLDER}/logo192.png`;
+
+  document.head.appendChild(link);
+  document.head.appendChild(appleLink);
+};
+
+const createMetaElements = () => {
+  const meta = document.createElement('meta');
+  meta.name = 'description';
+  meta.content = description;
+  document.head.appendChild(meta);
+};
+
+const createScriptElements = () => {
+  const script = document.createElement('script');
+  script.type = 'application/ld+json';
+
+  const json = {
+    '@context': 'https://schema.org',
+    '@type': 'Organization',
+    name: APP_NAME,
+    alternateName: `${APP_NAME}`,
+    url: 'https://mentorship.thapar.edu',
+    description,
+  };
+
+  script.innerHTML = JSON.stringify(json);
+
+  document.head.appendChild(script);
+};
+
+const setMetaData = () => {
+  document.title = APP_NAME;
+  createMetaElements();
+  createLinkElements();
+  createScriptElements();
+};
+
+const useMetaData = () => {
+  useEffect(() => {
+    setMetaData();
+  }, []);
+};
+
+export default useMetaData;
