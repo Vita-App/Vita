ff901fa31bd42bb6738f2258bb371cba07e6b945
Rishabh Malhotra <rishabhmalhotraa01@gmail.com>
2022-08-14 13:25:41 +0530

    update: mentorValues description

--- client/src/components/MentorValues/mentorValuesData.ts
+++ client/src/components/MentorValues/mentorValuesData.ts
@@ -9,7 +9,7 @@ const freeOfChargeValueData = {
 const vettedAndVerfiedValueData = {
   headline: 'Vetted & Verified',
   description:
-    'Mentors require a minimum of 5 years of experience in their respective industries to apply on the platform and a passion for mentoring.',
+    'Mentors require relevant experience in their respective industries to apply on the platform and a passion for mentoring.',
   image: '/Illustration/VettedVerified.svg',
   backgroundColor: '#753434bd',
 };
@@ -17,7 +17,7 @@ const vettedAndVerfiedValueData = {
 const diverseAndInclusiveValueData = {
   headline: 'Diverse & Inclusive',
   description:
-    'Over 4000 mentors from 60+ different countries, from various industries, and all walks of life.',
+    'Mentors spanning from 1970 Batch to the latest ,from various industries, from all walks of life.',
   image: '/Illustration/Diversity.svg',
   backgroundColor: '#284b63',
 };
