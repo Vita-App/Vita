6a1c08004c43ea760d562452e94fb91b4fc9b775
Rishabh Malhotra <rishabhmalhotraa01@gmail.com>
2022-08-24 18:51:45 +0530

    update: moving assets to cloudinary 🤳

--- client/src/data/swipeCards.ts
+++ client/src/data/swipeCards.ts
@@ -1,56 +1,26 @@
 export const swipeCardsInfo = [
-  // {
-  //   src: '/swipe_cards/making_better_tech_choices.png',
-  //   info: 'Making Better Tech Decisions',
-  // },
-
-  // {
-  //   src: '/swipe_cards/work_impact.png',
-  //   info: 'Presenting Work With Impact',
-  // },
-  // {
-  //   src: '/swipe_cards/product_management_carrer_path.png',
-  //   info: 'Getting into Product Management',
-  // },
-
-  // {
-  //   src: '/swipe_cards/talk.png',
-  //   info: 'Talk To Alumni',
-  // },
-  // {
-  //   src: '/swipe_cards/working_remotely.webp',
-  //   info: 'Working Remotely',
-  // },
-  // {
-  //   src: '/swipe_cards/user_research.webp',
-  //   info: 'How To Get Started With UI/UX',
-  // },
-  // {
-  //   src: '/swipe_cards/synergy.png',
-  //   info: 'Breaking Into Tech',
-  // },
   {
-    src: '/swipe_cards/compressed/imposter_syndrome.webp',
+    src: 'https://res.cloudinary.com/vita-app/image/upload/v1661346468/Vita/imposter_syndrome_bjwfnj.webp',
     info: 'Dealing with Imposter Syndrome',
   },
   {
-    src: '/swipe_cards/compressed/drawing.webp',
+    src: 'https://res.cloudinary.com/vita-app/image/upload/v1661346468/Vita/drawing_yojuo7.webp',
     info: 'Designing Your Carrer Path',
   },
   {
-    src: '/swipe_cards/compressed/acing_technical_interviews.webp',
+    src: 'https://res.cloudinary.com/vita-app/image/upload/v1661346468/Vita/acing_technical_interviews_rpka5i.webp',
     info: 'Acing Technical Interviews',
   },
   {
-    src: '/swipe_cards/compressed/getting_promoted.webp',
+    src: 'https://res.cloudinary.com/vita-app/image/upload/v1661346468/Vita/getting_promoted_qbju5f.webp',
     info: 'Getting Promoted',
   },
   {
-    src: '/swipe_cards/compressed/resume_review.webp',
+    src: 'https://res.cloudinary.com/vita-app/image/upload/v1661346469/Vita/resume_review_esizx3.webp',
     info: 'Resume Portfolio Review',
   },
   {
-    src: '/swipe_cards/compressed/getting_most_of_my_internship.webp',
+    src: 'https://res.cloudinary.com/vita-app/image/upload/v1661346468/Vita/getting_most_of_my_internship_dz0u1a.webp',
     info: 'Getting the most out of my Internship',
   },
 ];
