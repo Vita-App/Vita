b115689ba119bf1c45c0a94c4d36da27f8f32127
Rishabh Malhotra <rishabhmalhotraa01@gmail.com>
2022-09-14 03:55:50 +0530

    update: fix-slots

--- api/src/utils/sortExperiences.ts
+++ api/src/utils/sortExperiences.ts
@@ -5,9 +5,9 @@ export const sortExperiences = (experiences: ExperienceType[]) =>
 
 const experienceComparator = (a: ExperienceType, b: ExperienceType) => {
   if (a.end_year.trim().toLowerCase() === 'present') return 1;
-
+  if (b.end_year.trim().toLowerCase() === 'present') return -1;
   try {
-    return parseInt(a.end_year.trim(), 10) - parseInt(b.end_year.trim(), 10);
+    return parseInt(b.end_year.trim(), 10) - parseInt(a.end_year.trim(), 10);
   } catch (e) {
     return 0;
   }
