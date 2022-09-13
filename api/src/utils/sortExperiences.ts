d3548a72ca3f3360bd6f1f7ba48ed535c07f29fa
Rishabh Malhotra <rishabhmalhotraa01@gmail.com>
2022-09-14 04:12:20 +0530

    fix: sorting function

--- api/src/utils/sortExperiences.ts
+++ api/src/utils/sortExperiences.ts
@@ -4,10 +4,12 @@ export const sortExperiences = (experiences: ExperienceType[]) =>
   experiences.sort(experienceComparator);
 
 const experienceComparator = (a: ExperienceType, b: ExperienceType) => {
-  if (a.end_year.trim().toLowerCase() === 'present') return 1;
-  if (b.end_year.trim().toLowerCase() === 'present') return -1;
+  if (a.end_year.trim().toLowerCase() === 'present') return -1;
+  if (b.end_year.trim().toLowerCase() === 'present') return 1;
   try {
-    return parseInt(b.end_year.trim(), 10) - parseInt(a.end_year.trim(), 10);
+    return (
+      -1 * (parseInt(a.end_year.trim(), 10) - parseInt(b.end_year.trim(), 10))
+    );
   } catch (e) {
     return 0;
   }
