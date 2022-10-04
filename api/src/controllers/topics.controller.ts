9b56d22ded90c15d976f1d90083ef1adb4836f6f
xerneas965@gmail.com <icelain@pop-os.localdomain>
2022-10-04 22:15:55 +0530

    fetch topics data from mongodb

--- api/src/controllers/topics.controller.ts
+++ api/src/controllers/topics.controller.ts
@@ -1,7 +1,9 @@
 import { Request, Response } from 'express';
-import topicsData from '../data/topicsData';
+import { TopicModel } from '../Models/Topics';
+import { Topic } from '../types';
 
 const getTopics = async (req: Request, res: Response) => {
+  const topicsData: Topic[] = await TopicModel.find({});
   res.status(200).json(topicsData);
 };
 
