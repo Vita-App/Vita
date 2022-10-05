d07bc2d9bcd5dabfd0bb4a41bda538ea4bd92dbe
icelain <xerneas965@gmail.com>
2022-10-05 17:43:09 +0530

    made the limit 0 and corresponding changes

--- api/src/controllers/topics.controller.ts
+++ api/src/controllers/topics.controller.ts
@@ -2,7 +2,7 @@ import { Request, Response } from 'express';
 import { TopicModel } from '../Models/Topics';
 import { Topic } from '../types';
 
-// curl -X GET http://localhost:5000/api/get-topics?limit=15&page=1
+// curl -X GET http://localhost:5000/api/get-topics?limit=0&page=1
 const getTopics = async (req: Request, res: Response) => {
   const page = Number(req.query.page || '1');
 
@@ -10,18 +10,18 @@ const getTopics = async (req: Request, res: Response) => {
   let nextPage: number | null = null;
   let prevPage: number | null = null;
 
-  const limit = Number(req.query.limit) === 0 ? 15 : Number(req.query.limit);
+  const limit = Number(req.query.limit) !== 0 ? Number(req.query.limit) : 0;
 
   const searchOptions = {};
 
   const [topicsData, topicsLen] = await Promise.all([
-    TopicModel.find(searchOptions)
-      .skip(limit * (page - 1))
-      .limit(limit),
+    limit !== 0
+      ? TopicModel.find(searchOptions).skip(limit * (page - 1)).limit(limit)
+      : TopicModel.find(searchOptions),
     TopicModel.countDocuments(searchOptions),
   ]);
 
-  totalPages = Math.ceil(topicsLen / limit);
+  totalPages = limit !== 0 ? Math.ceil(topicsLen / limit) : 1;
   nextPage = page + 1 <= totalPages ? page + 1 : null;
   prevPage = page - 1 >= totalPages ? page - 1 : null;
 
